7fd788e115b24f66a6534417e85a6471d26969da
weytani <david@weytani.com>
2026-01-22 19:18:10 -0700

    add vampire

--- creatures/__init__.py
+++ creatures/__init__.py
@@ -1,5 +1,6 @@
 """Mythical creatures for TDD learning."""
 
 from creatures.unicorn.unicorn import Unicorn
+from creatures.vampire.vampire import Vampire
 
-__all__ = ["Unicorn"]
+__all__ = ["Unicorn", "Vampire"]
