2d671330828dea09be76f937c5ada9d053ac6704
weytani <david@weytani.com>
2026-01-22 19:53:03 -0700

    add hobbit and pirate

---
+++ creatures/hobbit/__init__.py
@@ -0,0 +1,5 @@
+"""Hobbit - A creature that introduces lifecycle modeling."""
+
+from creatures.hobbit.hobbit import Hobbit
+
+__all__ = ["Hobbit"]
---
+++ creatures/pirate/__init__.py
@@ -0,0 +1,5 @@
+"""Pirate - A creature that introduces state flags and accumulators."""
+
+from creatures.pirate.pirate import Pirate
+
+__all__ = ["Pirate"]
